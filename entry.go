6ccbc5144d98bd396e234bee1da7688bee3a2784
Sergio Salvatore <sergio@vimeo.com>
2023-03-08 17:45:06 -0500

    Support Structured Tags
    Adds `STags` to `Entry` and delegates marshalling those structures to
    the emitters in whatever format is most appropriate.
    
    Structured tags are added via the alog.AddStructuredTags(ctx, sTag1,
    sTag2...) function.

--- entry.go
+++ entry.go
@@ -4,9 +4,10 @@ import "time"
 
 // Entry is the struct passed to user-supplied formatters.
 type Entry struct {
-	Time time.Time
-	Tags [][2]string
-	File string
-	Line int
-	Msg  string
+	Time  time.Time
+	Tags  [][2]string
+	STags []STag
+	File  string
+	Line  int
+	Msg   string
 }

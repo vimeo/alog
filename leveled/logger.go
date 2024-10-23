4384b864c6d4629ab7b401b4d010c6f8be59c01d
Sergio Salvatore <sergio@vimeo.com>
2024-10-23 14:42:02 -0400

    Minimum Log Levels
    In the case of leveled logging, allow the user to set a minimum log
    level via the leveled.FilteredLogger type or through the
    gkelog.WithMinSeverity function which pipes it through the context.
    
    This allows us to suppress logs that are lower than the minimum level.

--- leveled/logger.go
+++ leveled/logger.go
@@ -7,6 +7,20 @@ import (
 	"github.com/vimeo/alog/v3"
 )
 
+// Level represents the severity of a log message.
+type Level uint8
+
+const (
+	Debug    Level = iota // debug
+	Info                  // info
+	Warning               // warning
+	Error                 // error
+	Critical              // critical
+)
+
+// LevelKey is the tag key associated with a level.
+const LevelKey = "level"
+
 // Logger is an interface that implements logging functions for different levels of severity.
 type Logger interface {
 	// Debug logs debugging or trace information.
@@ -25,8 +39,20 @@ type Logger interface {
 	Critical(ctx context.Context, f string, v ...interface{})
 }
 
+// FilteredLogger amends the Logger interface with a Log method that accepts the
+// level to log at.
+type FilteredLogger interface {
+	Logger
+	Log(ctx context.Context, level Level, f string, v ...interface{})
+	SetMinLevel(level Level)
+}
+
 type defaultLogger struct {
 	*alog.Logger
+
+	// Indicates the minimum level to log at.  If MinLevel is greater than the
+	// level of a given log message, the log message will be suppressed.
+	MinLevel Level
 }
 
 // Default returns a Logger that wraps the provided `alog.Logger`.
@@ -39,32 +65,49 @@ func Default(logger *alog.Logger) Logger {
 	}
 }
 
+// Filtered returns a logger that allows for setting the minimum level.
+func Filtered(logger *alog.Logger) FilteredLogger {
+	return &defaultLogger{
+		Logger: logger,
+	}
+}
+
 // Debug implements Logger.Debug
 func (d *defaultLogger) Debug(ctx context.Context, f string, v ...interface{}) {
-	ctx = alog.AddTags(ctx, "level", "debug")
-	d.Logger.Output(ctx, 3, fmt.Sprintf(f, v...))
+	d.Log(ctx, Debug, f, v...)
 }
 
 // Info implements Logger.Info
 func (d *defaultLogger) Info(ctx context.Context, f string, v ...interface{}) {
-	ctx = alog.AddTags(ctx, "level", "info")
-	d.Logger.Output(ctx, 3, fmt.Sprintf(f, v...))
+	d.Log(ctx, Info, f, v...)
 }
 
 // Warning implements Logger.Warning
 func (d *defaultLogger) Warning(ctx context.Context, f string, v ...interface{}) {
-	ctx = alog.AddTags(ctx, "level", "warning")
-	d.Logger.Output(ctx, 3, fmt.Sprintf(f, v...))
+	d.Log(ctx, Warning, f, v...)
 }
 
 // Error implements Logger.Error
 func (d *defaultLogger) Error(ctx context.Context, f string, v ...interface{}) {
-	ctx = alog.AddTags(ctx, "level", "error")
-	d.Logger.Output(ctx, 3, fmt.Sprintf(f, v...))
+	d.Log(ctx, Error, f, v...)
 }
 
 // Critical implements Logger.Critical
 func (d *defaultLogger) Critical(ctx context.Context, f string, v ...interface{}) {
-	ctx = alog.AddTags(ctx, "level", "critical")
-	d.Logger.Output(ctx, 3, fmt.Sprintf(f, v...))
+	d.Log(ctx, Critical, f, v...)
 }
+
+// Log implements FilteredLogger.Log
+func (d *defaultLogger) Log(ctx context.Context, level Level, f string, v ...interface{}) {
+	if level >= d.MinLevel {
+		ctx = alog.AddTags(ctx, LevelKey, level.String())
+		d.Logger.Output(ctx, 3, fmt.Sprintf(f, v...))
+	}
+}
+
+// SetMinLevel sets the minimum level that will be logged and implements FilteredLogger.
+func (d *defaultLogger) SetMinLevel(level Level) {
+	d.MinLevel = level
+}
+
+//go:generate go run golang.org/x/tools/cmd/stringer@latest -type Level -linecomment
